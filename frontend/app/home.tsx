2ed18ca407a44600406a951521f37d9cb46f14a7
jennnniferkuang <94874342+jennnniferkuang@users.noreply.github.com>
2025-09-13 23:51:45 -0400

    change to navigate function

--- frontend/app/home.tsx
+++ frontend/app/home.tsx
@@ -7,50 +7,33 @@ import HatersSection from '@/assets/images/HatersSection.svg';
 import StraysSection from '@/assets/images/StraysSection.svg';
 
 // other imports
-import { useRouter } from 'expo-router';
+import { router } from 'expo-router';
 import * as React from 'react';
 import { View } from 'react-native';
 import { Pressable } from 'react-native';
 import { TextBoxContainer } from '@/components/text-box-container';
 
+function navigateTo(path: string, event?: any) {
+  if (event) event.stopPropagation();
+  router.replace(path);
+}
+
 export default function HomeScreen() {
-
-  const router = useRouter();
-
-  const sendStray = (event: any) => {
-    event.stopPropagation();
-    router.replace('/send-stray');
-  };
-
-  const goToLeaderboard = (event: any) => {
-    event.stopPropagation();
-    router.replace('/leaderboard');
-  };
-
-  const goToHaters = (event: any) => {
-    event.stopPropagation();
-    router.replace('/haters');
-  };
-
-  const goToStrays = (event: any) => {
-    event.stopPropagation();
-    router.replace('/strays');
-  };
 
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Pressable onPress={goToStrays}>
+      <Pressable onPress={(event) => navigateTo('/strays', event)}>
         <StraysSection width="100%" style={{ position: 'absolute', top: -160, right: 0 }} />
       </Pressable>
-      <Pressable onPress={goToHaters}>
+      <Pressable onPress={(event) => navigateTo('/haters', event)}>
         <HatersSection width="100%" style={{ position: 'absolute', top: -250, right: 0 }} />
       </Pressable>
-      <Pressable onPress={goToLeaderboard}>
+      <Pressable onPress={(event) => navigateTo('/leaderboard', event)}>
         <LeaderboardSection width="100%" style={{ position: 'absolute', top: -375, right: 0 }} />
       </Pressable>
       <NewStraysSection width="100%" style={{ position: 'absolute', top: -30 }}/> 
       <TopBar width="100%" style={{ position: 'absolute', top: 25, left: -8}} /> 
-       <Pressable onPress={sendStray}>
+       <Pressable onPress={(event) => navigateTo('/send-stray', event)}>
         <SendButton style={{ alignSelf: 'center', position: 'absolute', top: -20 }} />
       </Pressable>
       <View style={{position: 'absolute', top: 100}}>
