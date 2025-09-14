f1153a0d4c9eb7b22ea85acf1bb2172149dee9d2
martendo <51766598+martendo@users.noreply.github.com>
2025-09-13 21:32:11 -0400

    Add thread/<int:id>/comeback url

--- backend/stray_sender_api/urls.py
+++ backend/stray_sender_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from stray_sender_api.views.thread import Thread
+from stray_sender_api.views.thread import Thread, ThreadComeback
 
 
 urlpatterns = [
     path('thread/<int:id>', Thread.as_view()),
+    path('thread/<int:id>/comeback', ThreadComeback.as_view()),
 ]
 
