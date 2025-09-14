860507e8f5966cfbd5b1465b94ad3c6a17c6b9fd
Belindq <unicornbelinda@gmail.com>
2025-09-14 00:47:46 -0400

    fixedgit add .git add .

--- frontend/app/home.tsx
+++ frontend/app/home.tsx
@@ -1,7 +1,9 @@
 // svgs
 import NewStraysSection from '@/assets/images/NewStraysSection.svg';
 import TopBar from '@/assets/images/topBar.svg';
+import SendButton from '@/assets/images/sendButton.svg';
 import LeaderboardSection from '@/assets/images/LeaderboardSection.svg';
+import HatersSection from '@/assets/images/HatersSection.svg';
 import StraysSection from '@/assets/images/StraysSection.svg';
 
 // other imports
@@ -20,28 +22,31 @@ export default function HomeScreen() {
 
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Pressable onPress={goToStrays}>
-        <StraysSection width="100%" style={{ position: 'absolute', top: -160, right: 0 }} />
-      </Pressable>
-      <Pressable onPress={goToHaters}>
+      
+      {/* <Pressable onPress={(event) => navigateTo('/haters', event)}>
         <HatersSection width="100%" style={{ position: 'absolute', top: -250, right: 0 }} />
+      </Pressable> */}
+    
+      <Pressable onPress={(event) => navigateTo('/leaderboard', event)}>
+        <LeaderboardSection width="100%" style={{ position: 'absolute', top: -140, right: 0 }} />
       </Pressable>
-      <Pressable onPress={goToLeaderboard}>
-        <LeaderboardSection width="100%" style={{ position: 'absolute', top: -375, right: 0 }} />
+       
+      <Pressable onPress={(event) => navigateTo('/strays', event)}>
+        <StraysSection width="100%" style={{ position: 'absolute', top: -270, right: 0 }} />
       </Pressable>
       <NewStraysSection width="100%" style={{ position: 'absolute', top: -30 }}/> 
       <TopBar width="100%" style={{ position: 'absolute', top: 25, left: -8}} /> 
-       <Pressable onPress={sendStray}>
+       {/* <Pressable onPress={(event) => navigateTo('/send-stray', event)}>
         <SendButton style={{ alignSelf: 'center', position: 'absolute', top: -20 }} />
       </Pressable> */}
-      <View style={{position: 'absolute', top: 210, right: 30, }}>      
-        <TextBoxContainer sender="user" message="Hello, world!" width={350} /> 
+      <View style={{position: 'absolute', top: 200, right: 30}}>
+        <TextBoxContainer sender="user" message="Hello, world!" width={350} />
       </View>
-      <View style={{position: 'absolute', top: 330, right: 30, }}>      
-        <TextBoxContainer sender="user" message="Hello, world!" width={350} /> 
+      <View style={{position: 'absolute', top: 320, right: 30}}>
+        <TextBoxContainer sender="user" message="Hello, world!" width={350} />
       </View>
-      <View style={{position: 'absolute', top: 450, right: 30, }}>      
-        <TextBoxContainer sender="user" message="Hello, world!" width={350} /> 
+      <View style={{position: 'absolute', top: 440, right: 30}}>
+        <TextBoxContainer sender="user" message="Hello, world!" width={350} />
       </View>
     </View>
   );
